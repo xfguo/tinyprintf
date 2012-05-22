2a6fa1e117f068fa238ffec8c5df45aca0b47f1b
Alex GUO <supereic@gmail.com>
2012-05-22 13:22:50 +0800

    * use cmake

---
+++ test/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void xputc ( void* p, char c)
+{
+	putchar(c);
+}
+int main() {
+	init_printf(NULL, xputc);
+	tfp_printf("jwofj%d %x %s wofj\n", 333, 333, "111");
+	return 0;
+}
